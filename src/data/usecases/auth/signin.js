f229f602039b52988f738c6718bf7ad6393176fa
Otávio Araújo <otaviotech@gmail.com>
2022-01-09 19:24:45 -0300

    feat: implement SignInUseCase

---
+++ src/data/usecases/auth/signin.js
@@ -0,0 +1,40 @@
+import { InvalidCredentialsError } from '../../../domain/errors';
+
+export class SignInUseCase {
+  #profileRepository;
+
+  #userRepository;
+
+  #authService;
+
+  constructor({ profileRepository, userRepository, authService }) {
+    this.#profileRepository = profileRepository;
+    this.#userRepository = userRepository;
+    this.#authService = authService;
+  }
+
+  async signin(input) {
+    let profile = await this.#profileRepository.findByEmail(input.identifier);
+
+    if (!profile) {
+      profile = await this.#profileRepository.findByUsername(input.identifier);
+    }
+
+    if (!profile) {
+      throw new InvalidCredentialsError();
+    }
+
+    const user = await this.#userRepository.findByProfileId(profile.id);
+
+    const passwordsMatch = await this.#authService.comparePasswords(
+      input.password,
+      user?.password || '',
+    );
+
+    if (!passwordsMatch) {
+      throw new InvalidCredentialsError();
+    }
+
+    return this.#authService.generateAuthToken({ id: user?.id });
+  }
+}
