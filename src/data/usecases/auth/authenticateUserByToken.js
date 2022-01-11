16cfa489894a9bd8fb7eeb2d4c267c5d2ff231f6
Otávio Araújo <otaviotech@gmail.com>
2022-01-11 16:25:24 -0300

    feat: implement AuthenticateUserByTokenUseCase

--- src/data/usecases/auth/authenticateUserByToken.js
+++ src/data/usecases/auth/authenticateUserByToken.js
@@ -3,18 +3,21 @@ import { InvalidCredentialsError } from '../../../domain/errors';
 export class AuthenticateUserByTokenUseCase {
   #authService;
 
-  constructor({ authService }) {
+  #userRepository;
+
+  constructor({ authService, userRepository }) {
     this.#authService = authService;
+    this.#userRepository = userRepository;
   }
 
   async authenticateUserByToken(token) {
-    const { isValid } = await this.#authService.validateAuthToken(token);
+    const { isValid, data } = await this.#authService.validateAuthToken(token);
 
     if (!isValid) {
       throw new InvalidCredentialsError();
     }
 
-    const user = await this.#authService.findUserByToken(token);
+    const user = await this.#userRepository.findById(data?.user.id);
 
     if (!user) {
       throw new InvalidCredentialsError();
