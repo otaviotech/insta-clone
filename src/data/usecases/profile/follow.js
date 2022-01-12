41af1c3d9bab8cbc27b2f79eea6bb0219ef80ea7
Otávio Araújo <otaviotech@gmail.com>
2022-01-12 11:54:21 -0300

    feat: implement FollowUseCase

---
+++ src/data/usecases/profile/follow.js
@@ -0,0 +1,36 @@
+import { ForbiddenError, ResourceNotFoundError } from '../../../domain/errors';
+
+export class FollowUseCase {
+  #profileRepository;
+
+  constructor({ profileRepository }) {
+    this.#profileRepository = profileRepository;
+  }
+
+  async follow(input) {
+    const profiles = await this.#profileRepository.findManyByIds([
+      input.profileId,
+      input.followerProfileId,
+    ]);
+
+    if (profiles.length === 0) {
+      throw new ResourceNotFoundError();
+    }
+
+    const isAlreadyFollowing = await this.#profileRepository.isFollowing({
+      followedId: input.profileId,
+      followerId: input.followerProfileId,
+    });
+
+    if (isAlreadyFollowing) {
+      throw new ForbiddenError();
+    }
+
+    const { status } = await this.#profileRepository.addFollower({
+      followedId: input.profileId,
+      followerId: input.followerProfileId,
+    });
+
+    return { status };
+  }
+}
