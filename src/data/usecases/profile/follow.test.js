7ee33d490c378171facec9902b952e1de4fbab31
Otávio Araújo <otaviotech@gmail.com>
2022-01-12 11:54:32 -0300

    test: implement FollowUseCase

---
+++ src/data/usecases/profile/follow.test.js
@@ -0,0 +1,94 @@
+import { jest } from '@jest/globals';
+import { ResourceNotFoundError, ForbiddenError } from '../../../domain/errors';
+import { FollowUseCase } from './follow';
+
+describe('FollowUseCase', () => {
+  const makeSut = () => {
+    const profileRepositoryStub = {
+      findManyByIds: jest.fn(),
+      isFollowing: jest.fn(),
+      addFollower: jest.fn(),
+    };
+
+    const sut = new FollowUseCase({
+      profileRepository: profileRepositoryStub,
+    });
+
+    const validInput = {
+      profileId: 1,
+      followerProfileId: 2,
+    };
+
+    return {
+      sut,
+      profileRepositoryStub,
+      validInput,
+    };
+  };
+
+  it('should throw a ResourceNotFoundError if any of the profiles are not found', async () => {
+    const { sut, profileRepositoryStub, validInput } = makeSut();
+
+    profileRepositoryStub.findManyByIds.mockResolvedValueOnce([]);
+
+    const promise = sut.follow(validInput);
+
+    expect(promise).rejects.toThrow(new ResourceNotFoundError());
+    expect(profileRepositoryStub.findManyByIds).toHaveBeenCalledWith([
+      validInput.profileId,
+      validInput.followerProfileId,
+    ]);
+  });
+
+  it("should throw a ForbiddenError if it's already following", (done) => {
+    const { sut, profileRepositoryStub, validInput } = makeSut();
+
+    profileRepositoryStub.findManyByIds.mockResolvedValueOnce([
+      { id: validInput.profileId },
+      { id: validInput.followerProfileId },
+    ]);
+
+    profileRepositoryStub.isFollowing.mockResolvedValueOnce(true);
+
+    sut
+      .follow(validInput)
+      .then(() => done.fail())
+      .catch((error) => {
+        expect(error).toEqual(new ForbiddenError());
+        expect(profileRepositoryStub.findManyByIds).toHaveBeenCalledWith([
+          validInput.profileId,
+          validInput.followerProfileId,
+        ]);
+        expect(
+          profileRepositoryStub.isFollowing({
+            followedId: validInput.profileId,
+            follwerId: validInput.followerProfileId,
+          }),
+        );
+
+        done();
+      });
+  });
+
+  it('should add the follower to the profile', async () => {
+    const { sut, profileRepositoryStub, validInput } = makeSut();
+    profileRepositoryStub.findManyByIds.mockResolvedValueOnce([
+      { id: validInput.profileId },
+      { id: validInput.followerProfileId },
+    ]);
+
+    profileRepositoryStub.isFollowing.mockResolvedValueOnce(false);
+    profileRepositoryStub.addFollower.mockResolvedValueOnce({
+      status: 'CONFIRMED',
+    });
+
+    const result = await sut.follow(validInput);
+
+    expect(profileRepositoryStub.addFollower).toHaveBeenCalledWith({
+      followedId: validInput.profileId,
+      followerId: validInput.followerProfileId,
+    });
+
+    expect(result).toEqual({ status: 'CONFIRMED' });
+  });
+});
