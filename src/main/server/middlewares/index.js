a3685e756567ff3bc47d3a7acd0ebf8c013d86de
Otávio Araújo <otaviotech@gmail.com>
2022-01-11 21:29:37 -0300

    docs: document signout route

--- src/main/server/middlewares/index.js
+++ src/main/server/middlewares/index.js
@@ -7,7 +7,13 @@ import { pinoHttpMiddleware } from './pino';
 export const registerMiddlewares = (app) => {
   app.use(ruid());
   app.use(pinoHttpMiddleware);
-  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        scriptSrc: ["'self'", 'blob:', "'unsafe-inline'", 'unpkg.com'],
+      },
+    }),
+  );
   app.use(bodyParser.json());
   app.use(cors());
 };
--- src/main/server/routes/apiSpec.js
+++ src/main/server/routes/apiSpec.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import cors from 'cors';
 import { AppEnv } from '../../env';
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', cors(), (req, res) => {
   res.sendFile('spec.html', { root: AppEnv.PUBLIC_DIR });
 });
 
